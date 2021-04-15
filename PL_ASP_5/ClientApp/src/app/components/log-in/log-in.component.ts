bcf57d7119ce62de40f20d0f45523ac4efac0e8b
Alox1d <tom.9997@mail.ru>
2021-04-15 11:57:18 +0300

    some refactor

--- PL_ASP_5/ClientApp/src/app/components/log-in/log-in.component.ts
+++ PL_ASP_5/ClientApp/src/app/components/log-in/log-in.component.ts
@@ -1,66 +1,73 @@
-import { AfterContentInit, AfterViewInit, Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { AfterContentInit, AfterViewInit, Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css']
 })
-export class LogInComponent implements AfterContentInit{
+export class LogInComponent implements AfterContentInit {
 
   Roles: any = ['Admin', 'Author', 'Reader'];
 
-  constructor(private http: HttpClient){ }
+  constructor(public http: HttpClient, @Inject('BASE_URL') public baseUrl: string) {
+  }
 
   ngAfterContentInit(): void {
     this.isAuth();
   }
-  postData(user: User){
-        
-      const body = {email: user.email, password: user.password, rememberMe: true,
-  returnUrl: "/"};
-      return this.http.post('https://localhost:44314/api/Account/Login', body); 
+
+
+  postData(user: User) {
+    const body = {
+      email: user.email, password: user.password, rememberMe: true,
+      returnUrl: "/"
+    };
+    return this.http.post(this.baseUrl + 'api/Account/Login', body);
   }
 
-  user: User=new User(); // данные вводимого пользователя
-      
-  receivedUser: User; // полученный пользователь
+  user: User = new User(); // данные вводимого пользователя
+
   done: boolean = false;
-  error:string;
-  submit(user: User){
-      this.postData(user)
-              .subscribe(
-                (data: any) => {
-                  this.done = true; this.error = data.message;
-                },
-                  error => { console.log(error)
-                  }
-              );
-  }
-  logout(){
-        
+  msg: string;
 
-    return this.http.post('https://localhost:44314/api/Account/LogOff', "")
-    .subscribe(
-      (data: any) => {
-        this.error = data.message;
-      },
-        error => { console.log(error)
+  submit(user: User) {
+    this.postData(user)
+      .subscribe(
+        (data: any) => {
+          this.done = true; this.msg = data.message;
+        },
+        error => {
+          console.log(error)
         }
-    );
-}
-isAuth(){
-        
+      );
+  }
+  logout() {
 
-  return this.http.post('https://localhost:44314/api/Account/isAuthenticated', "")
-  .subscribe(
-    (data: any) => {
-      this.error = data.message;
-    },
-      error => { console.log(error)
-      }
-  );
-}
+
+    return this.http.post(this.baseUrl + 'api/Account/LogOff', "")
+      .subscribe(
+        (data: any) => {
+          this.msg = data.message;
+        },
+        error => {
+          console.log(error)
+        }
+      );
+  }
+  isAuth() {
+
+
+    return this.http.post(this.baseUrl + 'api/Account/isAuthenticated', "")
+      .subscribe(
+        (data: any) => {
+          this.msg = data.message;
+        },
+        error => {
+          console.log(error)
+        }
+      );
+  }
 }
 
 class User {
--- PL_ASP_5/ClientApp/src/app/register/register.component.ts
+++ PL_ASP_5/ClientApp/src/app/register/register.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {Injectable} from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Component({
@@ -13,24 +12,25 @@ export class RegisterComponent {
 
   Roles: any = ['Admin', 'Author', 'Reader'];
 
-  constructor(private http: HttpClient){ }
+  constructor(public http: HttpClient, @Inject('BASE_URL') public baseUrl: string) {
+  }
  
   postData(user: User){
         
       const body = {email: user.email, password: user.password, passwordConfirm: user.passwordConfirm};
-      return this.http.post('https://localhost:44314/api/Account/Register', body); 
+      return this.http.post(this.baseUrl + 'api/Account/Register', body); 
   }
 
   user: User=new User(); // данные вводимого пользователя
       
   receivedUser: User; // полученный пользователь
   done: boolean = false;
-  error:string;
+  msg:string;
   submit(user: User){
       this.postData(user)
               .subscribe(
                 (data: any) => {
-                  this.done = true; this.error = data.message;
+                  this.done = true; this.msg = data.message;
                 },
                   error => { console.log(error)
                   }
